b92f999f8c3a03b99e1294014d42975cef8300a4
Jahirul Islam <rajumjib@aol.com>
2017-11-09 22:17:30 +0600

    New file type added

--- tags/index.js
+++ tags/index.js
@@ -10,84 +10,126 @@ var added = 'added{{name}}:{{location}}:{{tagName}}';
 var DEFAULTS = {
     STARTS: {
         html: '<!-- ' + start + ' -->',
-        css: '/* ' + start + ' */',
+
+        cs: '// ' + start + '',
         js: '// ' + start + '',
+
         jsx: '{/* ' + start + ' */}',
+
         jade: '//- ' + start + '',
         pug: '//- ' + start + '',
+
         slm: '/ ' + start + '',
         slim: '/ ' + start + '',
+
         haml: '-# ' + start + '',
+
+        css: '/* ' + start + ' */',
         less: '/* ' + start + ' */',
         sass: '/* ' + start + ' */',
         scss: '/* ' + start + ' */'
     },
     ENDS: {
         html: '<!-- ' + end + ' -->',
-        css: '/* ' + end + ' */',
+
+        cs: '// ' + end + '',
         js: '// ' + end + '',
+
         jsx: '{/* ' + end + ' */}',
+
         jade: '//- ' + end + '',
         pug: '//- ' + end + '',
+
         slm: '/ ' + end + '',
         slim: '/ ' + end + '',
+
         haml: '-# ' + end + '',
+
+        css: '/* ' + end + ' */',
         less: '/* ' + end + ' */',
         sass: '/* ' + end + ' */',
         scss: '/* ' + end + ' */'
     },
     APPENDS: {
         html: '<!-- ' + append + ' -->',
-        css: '/* ' + append + ' */',
+
+        cs: '// ' + append + '',
         js: '// ' + append + '',
+
         jsx: '{/* ' + append + ' */}',
+
         jade: '//- ' + append + '',
         pug: '//- ' + append + '',
+
         slm: '/ ' + append + '',
         slim: '/ ' + append + '',
+
         haml: '-# ' + append + '',
+
+        css: '/* ' + append + ' */',
         less: '/* ' + append + ' */',
         sass: '/* ' + append + ' */',
         scss: '/* ' + append + ' */'
     },
     ADDEDS: {
         html: '<!-- ' + added + ' -->',
-        css: '/* ' + added + ' */',
+
+        cs: '// ' + added + '',
         js: '// ' + added + '',
+
         jsx: '{/* ' + added + ' */}',
+
         jade: '//- ' + added + '',
         pug: '//- ' + added + '',
+
         slm: '/ ' + added + '',
         slim: '/ ' + added + '',
+
         haml: '-# ' + added + '',
+
+        css: '/* ' + added + ' */',
         less: '/* ' + added + ' */',
         sass: '/* ' + added + ' */',
         scss: '/* ' + added + ' */'
     },
     BEGINLINES: {
         html: 1,
-        css: 2,
+
+        cs: 0,
         js: 2,
+
         jsx: 0,
+
         jade: 0,
         pug: 0,
+
         slm: 0,
         slim: 0,
+
         haml: 0,
+
+        css: 2,
         less: 2,
         sass: 2,
         scss: 0
     },
     ENDLINES: {
         html: 0,
-        css: 1,
+
+        cs: 0,
         js: 1,
+
         jsx: 0,
+
         jade: 0,
         pug: 0,
+
         slm: 0,
         slim: 0,
+
         haml: 0,
+
+        css: 1,
         less: 1,
         sass: 1,
         scss: 0
